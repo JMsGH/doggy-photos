69c5a1be4b0d42bcf244c3cfc83b0aeff4e77467
JMsGH <10666028+JMsGH@users.noreply.github.com>
2021-01-25 11:25:37 +0000

    added animation to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     <div class="container">
         <div class="row align-items-center mb-5">
-            <div class="col-sm-1"></div>
-            <div class="col-sm-5">
+            <div class="col-sm-1 col-md-1"></div>
+            <div class="col-sm-5 col-md-5">
                 <img src="https://dog-photos-bucket.s3-ap-northeast-1.amazonaws.com/welcome_page_img.png" class="profile-img rounded-circle img-thumbnail border-0 animated rollIn">
             </div>
             <div class="col-sm-6 align-items-center">
@@ -29,12 +29,12 @@
         <div class="scroll-text">
             <h3 class="text-center mb-4 text-info">このサイトについて</h3>
             <div class="text-center mt-4 about-text">
-            <p><i class="fas fa-paw awesome"></i> 愛犬の写真を投稿して他のユーザに公開できます。</p>
-            <p><i class="fas fa-paw awesome"></i> 他のユーザをフォローできます。</p>
-            <p><i class="fas fa-paw awesome"></i> 写真をお気に入りとして登録できます。</p>
-            <p><i class="fas fa-paw awesome"></i> フィラリア予防の投薬スケジュールを登録できます。</p>
-            <p><i class="fas fa-paw awesome"></i> 愛犬を登録できます。</p>
-            <p><i class="fas fa-paw awesome"></i> 登録した愛犬ごとに体重を記録しグラフを表示できます。</p>
+            <p class="animated slideInLeft"><i class="fas fa-paw awesome"></i> 愛犬の写真を投稿して他のユーザに公開できます。</p>
+            <p class="animated slideInRight"><i class="fas fa-paw awesome"></i> 他のユーザをフォローできます。</p>
+            <p class="animated slideInLeft"><i class="fas fa-paw awesome"></i> 写真をお気に入りとして登録できます。</p>
+            <p class="animated slideInRight"><i class="fas fa-paw awesome"></i> フィラリア予防の投薬スケジュールを登録できます。</p>
+            <p class="animated slideInLeft"><i class="fas fa-paw awesome"></i> 愛犬を登録できます。</p>
+            <p class="animated slideInRight"><i class="fas fa-paw awesome"></i> 登録した愛犬ごとに体重を記録しグラフを表示できます。</p>
             </div>
         </div>
     </div>

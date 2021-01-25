62910dfa3d0e2fc1d699cb556ccc1de6756a35b2
JMsGH <10666028+JMsGH@users.noreply.github.com>
2021-01-25 08:11:26 +0000

    added about this site to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,21 +10,40 @@
             <div class="col-sm-6 align-items-center">
                 <h1 class="text-center animated bounceInDown">犬フォト！ <i class="fas fa-paw awesome"></i></h1>
             </div>
-        </div>
+            </div>
         
-        <div class="mt-5 text-center">
-        {{-- ユーザ登録ページへのリンク --}}
-        {!! link_to_route('signup.get', '登録する', [], ['class' => 'btn btn-primary btn-default']) !!}
-        </div>
+            <div class="mt-5 text-center">
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', '登録する', [], ['class' => 'btn btn-primary btn-default']) !!}
+            </div>
         
-        <div class="mt-5 text-center">
-        {{-- ログインページへのリンク --}}
-        {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-info btn-default']) !!}
+            <div class="mt-5 text-center mb-5">
+            {{-- ログインページへのリンク --}}
+            {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-info btn-default']) !!}
+            </div>
         </div>
-
-
-        
     </div>
+
+    <div class="s-body">
+    <div class="wrapper mt-5">
+        <div class="scroll-text">
+            <h3 class="text-center mb-4 text-info">このサイトについて</h3>
+            <div class="text-center mt-4 about-text">
+            <p><i class="fas fa-paw awesome"></i> 愛犬の写真を投稿して他のユーザに公開できます。</p>
+            <p><i class="fas fa-paw awesome"></i> 他のユーザをフォローできます。</p>
+            <p><i class="fas fa-paw awesome"></i> 写真をお気に入りとして登録できます。</p>
+            <p><i class="fas fa-paw awesome"></i> フィラリア予防の投薬スケジュールを登録できます。</p>
+            <p><i class="fas fa-paw awesome"></i> 愛犬を登録できます。</p>
+            <p><i class="fas fa-paw awesome"></i> 登録した愛犬ごとに体重を記録しグラフを表示できます。</p>
+            </div>
+        </div>
+    </div>
+    </div>
+
+
+
+        
+    
     
 
     
